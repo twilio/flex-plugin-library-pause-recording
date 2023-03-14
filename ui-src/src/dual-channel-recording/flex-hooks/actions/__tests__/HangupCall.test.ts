77bbdd53dc56c500e10e64fc4217d26fb81e1ebb
susaha135 <susaha@twilio.com>
2023-03-14 16:06:52 +0530

    restructuring the test files

---
+++ ui-src/src/dual-channel-recording/flex-hooks/actions/__tests__/HangupCall.test.ts
@@ -0,0 +1,31 @@
+import * as Flex from '@twilio/flex-ui';
+import { handleDualChannelHangupCall } from '../HangupCall'
+import '@testing-library/jest-dom'
+import * as dualChannel from "../../../helpers/dualChannelHelper";
+
+jest.mock('../../../helpers/dualChannelHelper', ()=>{
+  return {
+    addMissingCallDataIfNeeded : jest.fn()
+  }
+}) ;
+
+describe('handleDualChannelCompleteTask', () => {
+  let flex: typeof Flex = Flex;
+  let manager: Flex.Manager = Flex.Manager.getInstance();
+  const actionSpy = jest.spyOn(dualChannel, 'addMissingCallDataIfNeeded');
+  it('should call addMissingCallDataIfNeeded', async () => {
+      //handleDualChannelCompleteTask(flex, manager);
+    const addListenerSpy = jest.spyOn(Flex.Actions, 'addListener');
+
+    await handleDualChannelHangupCall(flex, manager);
+    const payload = {
+      task:{
+      attributes: {conference : '12345'}
+      }
+    }
+    flex.Actions.invokeAction('HangupCall', payload)
+    await expect(addListenerSpy).toHaveBeenCalledTimes(1);
+    expect(actionSpy).toBeCalledWith(payload.task);
+    await expect(actionSpy).toHaveBeenCalled();
+    });
+})  
---
+++ ui-src/src/pause-recording/flex-hooks/components/__tests__/CallCanvas.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import * as Flex from '@twilio/flex-ui';
+import { addPauseStatusPanel } from '../CallCanvas';
+//import { Manager } from '@twilio/flex-ui';
+import { ErrorManager, FlexErrorSeverity, FlexPluginErrorType } from "../../../../utils/ErrorManager";
+
+
+describe('CallCanvas', () => {
+    let flex: typeof Flex = Flex;
+
+    //let manager: Flex.Manager = Flex.Manager.getInstance();
+    const addContentSpy = jest.spyOn(Flex.CallCanvas.Content, 'add');
+    
+    it('adds Pause Status Panel to call canvas', () => {
+        addPauseStatusPanel(flex);
+      expect(addContentSpy).toHaveBeenCalledTimes(1);
+    });
+
+  });
+  
